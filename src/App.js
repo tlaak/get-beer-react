67d9880781b04083cf6c6831cb2ca6b3724e7814
Timo Laak <tlaak@users.noreply.github.com>
2018-08-20 21:37:09 +0100

    Use the GetBeer component and prevent it from crashing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import GetBeer from "./components/GetBeer";
 
 class App extends Component {
   render() {
     return (
       <main className="app">
         <h1 className="app__heading">Welcome to the Get Beer app!</h1>
-        <app-get-beer />
+        <GetBeer
+          isLoading={false}
+          beers={[]}
+          getBeer={() => console.log("Get beer clicked")}
+        />
       </main>
     );
   }
