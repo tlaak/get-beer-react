1e677e04e8d2851feee3933e0b250708d25eec27
Timo Laak <timo@thisisbud.com>
2018-08-23 16:26:56 +0100

    Export the function directly

--- src/services/beerService.ts
+++ src/services/beerService.ts
@@ -1,8 +1,6 @@
 import axios, { AxiosResponse } from 'axios'
 
-const getBeer = (): Promise<AxiosResponse> => {
+export const getBeer = (): Promise<AxiosResponse> => {
   const beerApiEndpoint = 'https://api.punkapi.com/v2/beers'
   return axios.get(beerApiEndpoint)
 }
-
-export default getBeer
