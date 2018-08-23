402453501ba7d3344a900cac98fc4e8e38d76374
Timo Laak <timo@thisisbud.com>
2018-08-23 16:31:18 +0100

    Added beer type, removed semicolons

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -7,19 +7,41 @@
  */
 
 /* SystemJS module definition */
-declare var module: NodeModule;
+declare var module: NodeModule
 interface NodeModule {
-  id: string;
+  id: string
 }
 
 interface ActionWithPayload<T> {
-  type: string;
-  payload: T;
+  type: string
+  payload: T
 }
 
-interface Beer {}
+interface Beer {
+  id: number
+  name: string
+  tagline: string
+  first_brewed: string
+  description: string
+  image_url: string
+  abv: number
+  ibu: number
+  target_fg: number
+  target_og: number
+  ebc: number
+  srm: number
+  ph: number
+  attenuation_level: number
+  volume: any
+  boil_volume: any
+  method: any
+  ingredients: any
+  food_pairing: any
+  brewers_tips: string
+  contributed_by: string
+}
 
 interface BeerState {
-  isLoading: boolean;
-  beers: Beer[];
+  isLoading: boolean
+  beers: Beer[]
 }
