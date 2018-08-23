bba383a503f0993bcbdbb5154b5c7a10991483d4
Timo Laak <timo@thisisbud.com>
2018-08-23 16:31:24 +0100

    Correct return type

--- src/services/beerService.ts
+++ src/services/beerService.ts
@@ -1,6 +1,6 @@
-import axios, { AxiosResponse } from 'axios'
+import axios, { AxiosPromise } from 'axios'
 
-export const getBeer = (): Promise<AxiosResponse> => {
+export const getBeer = (): AxiosPromise<Beer[]> => {
   const beerApiEndpoint = 'https://api.punkapi.com/v2/beers'
   return axios.get(beerApiEndpoint).then(response => response.data)
 }
