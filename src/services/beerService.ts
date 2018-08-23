3c2eb62590927bd3ff73dfe9f495733500e14b30
Timo Laak <timo@thisisbud.com>
2018-08-23 16:27:12 +0100

    Take the data, not the full response

--- src/services/beerService.ts
+++ src/services/beerService.ts
@@ -2,5 +2,5 @@ import axios, { AxiosResponse } from 'axios'
 
 export const getBeer = (): Promise<AxiosResponse> => {
   const beerApiEndpoint = 'https://api.punkapi.com/v2/beers'
-  return axios.get(beerApiEndpoint)
+  return axios.get(beerApiEndpoint).then(response => response.data)
 }
